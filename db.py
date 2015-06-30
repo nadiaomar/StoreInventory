ff8ce957487289434f523f5dc56b1cc523674053
Nadia Omar <nadiaomar1998@gmail.com>
2015-06-30 14:40:23 -0400

    detect max on hand

--- db.py
+++ db.py
@@ -3,7 +3,10 @@ import psycopg2
 try:
     conn = psycopg2.connect("dbname='nhdinventory' user='postgres' host='localhost' password='test123'")
     cur = conn.cursor()
-    cur.execute("INSERT INTO Inventory(onhand, instock) VALUES (3, 22);")
+
+    command = "INSERT INTO Inventory(onhand, instock) VALUES (3, 22);"
+    print(command)
+    cur.execute(command)
 
     # rows = cur.fetchall()
 
@@ -11,7 +14,6 @@ try:
     # 	print row
 
     conn.commit()
-
 
 except:
     print "I am unable to connect to the database"
